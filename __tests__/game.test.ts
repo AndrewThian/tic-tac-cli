96b8e9ae4f6ab6e3cde4c77debed87fed186637e
AndrewThian <andrewbobbythian@gmail.com>
2018-11-21 01:11:52 +0800

    test recursive horizontal check

--- __tests__/game.test.ts
+++ __tests__/game.test.ts
@@ -1,7 +1,8 @@
 import { Game } from "../lib/game";
 import { Board } from "../lib/board";
+import { Traverse } from "../lib/traverse"
 
-describe("Game testsuite", () => {
+describe("<Game> clas test suite", () => {
     const boardSize = 3
     test("initialize with Board", () => {
         const board = new Board(boardSize, 3)
@@ -18,41 +19,119 @@ describe("Game testsuite", () => {
         const game = new Game(board);
         game.play(10, "x")
     })
-    describe("Class Game #play test suite", () => {
-        let MockedBoard: any;
-        let board: any;
-        let game: Game;
+    describe("#checkWin", () => {
         beforeEach(() => {
-            jest.mock("../lib/board", () => {
-                return function () {
-                    return {
-                        maxGridNumber: jest.fn(() => boardSize * boardSize),
-                        convertInputToCoordinates: jest.fn(() => [ 0, 0 ]),
-                        markSquare: jest.fn(() => {}),
-                        print: jest.fn(() => {})
-                    }
-                }
-            })
-            MockedBoard = require("../lib/board");
-            board = new MockedBoard();
-            game = new Game(board);
-
-            game.play(1, "x");
+            console.log = jest.fn(() => {})
         })
         afterEach(() => {
             jest.clearAllMocks();
         })
-        test("#play should call #board.convertInputToCoordinates", () => {
-            expect(board.convertInputToCoordinates).toBeCalledTimes(1)
+        test("#play should execute #checkWin on every call", () => {
+            const board = new Board(3)
+            const game = new Game(board)
+            game.checkWin = jest.fn(() => {})
+
+            game.play(1, "x")
+            game.play(2, "x")
+            expect(game.checkWin).toBeCalledTimes(2);
         })
-        test("#play should call #board.maxGridNumber", () => {
-            expect(board.maxGridNumber).toBeCalledTimes(1)
+        test("should return true if matched win condition", () => {
+            const winCondition = 3
+            const board = new Board(3)
+            const game = new Game(board)
+            // mock horizontal check to win condition
+            game.horizontal = jest.fn(() => 3)
+            expect(game.checkWin("x", 0, 0, winCondition)).toBeTruthy()
         })
-        test("#play should call #board.markSquare", () => {
-            expect(board.markSquare).toBeCalledTimes(1)
+        test("should return false if not win condition", () => {
+            const winCondition = 3
+            const board = new Board(3)
+            const game = new Game(board)
+            // mock horizontal check to win condition
+            game.horizontal = jest.fn(() => 2)
+            expect(game.checkWin("x", 0, 0, winCondition)).toBeFalsy()
         })
-        test("#play should call #board.print", () => {
-            expect(board.print).toBeCalledTimes(1)
+    })
+    describe("#horizontal", () => {
+        test("should traverse left and right", () => {
+            const mockLeft = jest.spyOn(Traverse, "left")
+            const mockRight = jest.spyOn(Traverse, "right")
+            const board = new Board(3)
+            board.markSquare("x", 1, 1)
+            const game = new Game(board)
+
+            game.horizontal("x", 1, 1, new Set());
+            expect(mockLeft).toBeCalled();
+            expect(mockRight).toBeCalled();
+        })
+        describe("recursive test cases", () => {
+            test("should called recursively 2x on 1 play", () => {
+                const board = new Board(boardSize)
+                const game = new Game(board)
+                const mock = jest.spyOn(game, "horizontal")
+                game.play(1, "x")
+    
+                expect(mock).toBeCalledTimes(2)
+            })
+            test("should be called recusively 5x on 2 play", () => {
+                const board = new Board(boardSize)
+                const game = new Game(board)
+                const mock = jest.spyOn(game, "horizontal")
+                game.play(1, "x")
+                game.play(2, "x")
+
+                expect(mock).toBeCalledTimes(5)
+            })
+            test("should be called recusively 9x on 3 play", () => {
+                const board = new Board(boardSize)
+                const game = new Game(board)
+                const mock = jest.spyOn(game, "horizontal")
+                game.play(1, "x")
+                game.play(2, "x")
+                game.play(3, "x")
+
+                expect(mock).toBeCalledTimes(9)
+            })
+        })
+    })
+    describe("#play test scope", () => {
+        describe("Should call <Board> class methods", () => {
+            let MockedBoard: any;
+            let board: any;
+            let game: Game;
+            beforeEach(() => {
+                jest.mock("../lib/board", () => {
+                    return function () {
+                        return {
+                            valueFromCoordinates: jest.fn(() => ""),
+                            maxGridNumber: jest.fn(() => boardSize * boardSize),
+                            convertInputToCoordinates: jest.fn(() => [ 0, 0 ]),
+                            markSquare: jest.fn(() => {}),
+                            print: jest.fn(() => {})
+                        }
+                    }
+                })
+                MockedBoard = require("../lib/board");
+                board = new MockedBoard();
+                game = new Game(board);
+
+                game.play(1, "x");
+            })
+            afterEach(() => {
+                jest.clearAllMocks();
+            })
+            test("#play should call #board.convertInputToCoordinates", () => {
+                expect(board.convertInputToCoordinates).toBeCalledTimes(1)
+            })
+            test("#play should call #board.maxGridNumber", () => {
+                expect(board.maxGridNumber).toBeCalledTimes(1)
+            })
+            test("#play should call #board.markSquare", () => {
+                expect(board.markSquare).toBeCalledTimes(1)
+            })
+            test("#play should call #board.print", () => {
+                expect(board.print).toBeCalledTimes(1)
+            })
         })
     })
 })
--- lib/game.ts
+++ lib/game.ts
@@ -4,7 +4,7 @@ import { Traverse } from "./traverse";
 
 export interface IGame {
     readonly board: IBoard
-    play(inputNumber: number, symbol: Mark): void
+    play(inputNumber: number, symbol: Mark): boolean
     checkWin(symbol: Mark, row: number, col: number, winCondition: number): boolean
 }
 
@@ -15,7 +15,8 @@ export class Game implements IGame {
         this.board = board
     }
 
-    play(inputNumber: number, symbol: Mark) {
+    play(inputNumber: number, symbol: Mark): boolean {
+
         if (inputNumber > this.board.maxGridNumber()) {
             console.warn("woops, invalid board number")
             return
@@ -26,9 +27,15 @@ export class Game implements IGame {
         this.board.markSquare(symbol, row, col);
         // print board
         this.board.print();
+        if (this.checkWin(symbol, row, col, this.board.winCondition)) {
+            console.log("win")
+            return true
+        }
+        return false
     }
 
     checkWin(symbol: Mark, row: number, col: number, winCondition: number): boolean {
+        console.log("GOING INTO FIRST CALL")
         let horizontalCheck = this.horizontal(symbol, row, col, new Set())
         return horizontalCheck >= winCondition
     }
@@ -44,12 +51,15 @@ export class Game implements IGame {
         let [leftRow, leftCol] = Traverse.left(row, col)
         let [rightRow, rightCol] = Traverse.right(row, col)
         // check left
+        console.log(seen)
         if (leftCol >= 0 && !seen.has(`${leftRow}-${leftCol}`)) {
+            console.log("LEFT")
             const left = self.horizontal(symbol, leftRow, leftCol, seen)
             counter += left;
         }
         // check right
         if (rightCol >= 0 && !seen.has(`${rightRow}-${rightCol}`)) {
+            console.log("RIGHT")
             const right = self.horizontal(symbol, rightRow, rightCol, seen)
             counter += right
         }
