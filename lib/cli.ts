1e3e6dc56a512d784b43c415e20b5eb3ba974815
AndrewThian <andrewbobbythian@gmail.com>
2018-11-21 14:22:08 +0800

    refactored index.ts into cli class, test suite WIP

---
+++ lib/cli.ts
@@ -0,0 +1,108 @@
+import readline from "readline";
+import EventEmitter from "events"
+import { Player } from "./player";
+import { Board } from "./board";
+import { Game } from "./game";
+import { State } from "./state";
+
+interface ICLI {
+    rl: readline.Interface
+    config: {
+        input: NodeJS.ReadStream
+        output: NodeJS.WritableStream
+        terminal: boolean
+    }
+    prefix: string
+    getPlayerData(): void;
+    startGame(p1: Player, p2: Player): void;
+    questionPlayer(state: State): void;
+    handleInputValue(inputValue: number, game: Game, state: State): void;
+}
+
+export class CLI extends EventEmitter implements ICLI {
+    rl: readline.Interface
+    config: {
+        input: NodeJS.ReadStream
+        output: NodeJS.WritableStream
+        terminal: boolean
+    }
+    prefix: string
+
+    constructor() {
+        super()
+        this.config = {
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+        }
+        this.prefix = ">> "
+        this.rl = readline.createInterface(this.config)
+        this.rl.setPrompt(this.prefix)
+    }
+
+    getPlayerData() {
+        console.log("called")
+        this.rl.question("Enter name of player 1 \n>> ", (p1name: string) => {
+            const p1 = new Player(1, p1name)
+            console.log(`Welcome ${p1.name}, symbol: ${p1.symbol}`)
+            this.rl.question("Enter name of player 2 \n>> ", (p2name: string) => {
+                const p2 = new Player(2, p2name)
+                console.log(`Welcome ${p2.name}, symbol: ${p2.symbol}`)
+                this.emit("players", { p1, p2 })
+            })
+        })
+    }
+
+    startGame(p1: Player, p2: Player) {
+        this.rl.question("Enter board size \n>> ", (size: string) => {
+            const n = parseInt(size, 10)
+            if (isNaN(n)) {
+                console.warn("invalid board size")
+                this.rl.close();
+                return
+            }
+            const board = new Board(n)
+            const game = new Game(board)
+            const state = new State(p1, p2)
+
+            console.log("\n");
+            board.print();
+            console.log("\n");
+
+            this.emit("new game", { p1, p2, game, state })
+        })
+    }
+
+    handleInputValue(inputNumber: number, game: Game, state: State) {
+        const currentPlayer = state.currentPlayer();
+        switch(game.play(inputNumber, currentPlayer.symbol)) {
+            case "next": {
+                state.nextTurn();
+                this.questionPlayer(state);
+                break;
+            }
+            case "invalid": {
+                this.questionPlayer(state);
+                break;
+            }
+            case "draw": {
+                console.log("DRAW!")
+                this.rl.close();
+            }
+            case "win": {
+                console.log(`Congrats! ${currentPlayer.name}! You've won.`);
+                this.rl.close();
+                break;
+            }
+        }
+    }
+
+    questionPlayer(state: State) {
+        this.rl.setPrompt(
+            `${state.currentPlayer().name}, choose a box to place an '${
+                state.currentPlayer().symbol
+            }' \n >> `
+        );
+        this.rl.prompt();
+    }
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,137 +1,30 @@
-import readline from "readline";
-import EventEmitter from "events";
 import { Player } from "./player";
-import { Board, IBoard } from "./board";
 import { Game } from "./game";
+import { CLI } from "./cli";
+import { State } from "./state";
 
-const _event = new EventEmitter();
-const rlConfig = {
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false,
-};
+const cli = new CLI();
 
-const rl = readline.createInterface(rlConfig);
-const prefix = ">> ";
-rl.setPrompt(prefix);
+cli.getPlayerData();
 
-interface GameData {
-    p1: Player;
-    p2: Player;
-    board: IBoard;
-    game: Game;
-}
+cli.on("players", ({ p1, p2 }: { p1: Player, p2: Player }) => {
+    cli.startGame(p1, p2)
+})
 
-const setPlayers = () => {
-    rl.question("Enter name of player 1 \n>> ", (name: string) => {
-        const p1 = new Player(1, name);
-        console.log(`P1: ${p1.name}, symbol: ${p1.symbol}`);
-        rl.prompt();
-        rl.question("Enter name of player 2 \n>> ", (name: string) => {
-            const p2 = new Player(2, name);
-            console.log(`P1: ${p2.name}, symbol: ${p2.symbol}`);
-            rl.prompt();
-            rl.question("Enter board size \n>> ", (size: string) => {
-                const board = new Board(parseInt(size, 10));
-                const game = new Game(board);
-                _event.emit("start", {
-                    p1,
-                    p2,
-                    board,
-                    game,
-                });
-            });
-        });
-    });
-};
+cli.on("new game", ({ p1, p2, game, state }: { p1: Player, p2: Player, game: Game, state: State }) => {
+    cli.questionPlayer(state)
 
-type GameplayStatus = "start" | "init" | "turn";
-
-class State {
-    playerPool: Player[];
-    currentTurn: number;
-    currentState: GameplayStatus;
-
-    constructor(
-        p1: Player,
-        p2: Player,
-        currentTurn: number = 2,
-        currentState: GameplayStatus = "init"
-    ) {
-        this.playerPool = [p1, p2];
-        this.currentTurn = currentTurn;
-        this.currentState = currentState;
-    }
-
-    currentPlayer = (): Player => {
-        return this.playerPool[this.currentTurn % 2];
-    };
-
-    nextTurn = (): void => {
-        this.currentTurn += 1;
-    };
-
-    currentGameStatus = (): GameplayStatus => {
-        return this.currentState;
-    };
-}
-
-const questionPlayer = (gameState: State) => {
-    rl.setPrompt(
-        `${gameState.currentPlayer().name}, choose a box to place an '${
-            gameState.currentPlayer().symbol
-        }' \n >> `
-    );
-    rl.prompt();
-};
-
-_event.on("start", ({ p1, p2, game, board }: GameData) => {
-    board.print();
-
-    const gameState = new State(p1, p2);
-    questionPlayer(gameState);
-
-    rl.on("line", (value: string) => {
+    cli.rl.on("close", () => {
+        process.exit();
+    })
+    cli.rl.on("line", (value: string) => {
         if (value.match(/^[0-9]+$/)) {
-            _event.emit("turn", { gameState, board, game, value });
+            const inputValue = parseInt(value)
+            cli.handleInputValue(inputValue, game, state)
         } else if (value === "exit") {
-            rl.close();
+            cli.rl.close()
+        } else if (value === "restart") {
+            console.log(value)
         }
-    }).on("close", () => {
-        process.exit(0);
-    });
-});
-
-interface PlayerTurnData {
-    gameState: State;
-    board: Board;
-    game: Game;
-    value: string;
-}
-
-_event.on("turn", ({ gameState, game, value }: PlayerTurnData) => {
-    const inputNumber = parseInt(value);
-    const currentPlayer = gameState.currentPlayer();
-    switch (game.play(inputNumber, currentPlayer.symbol)) {
-        case "next": {
-            gameState.nextTurn();
-            questionPlayer(gameState);
-            break;
-        }
-        case "invalid": {
-            questionPlayer(gameState);
-            break;
-        }
-        case "draw": {
-            console.log("DRAW");
-            rl.close();
-        }
-        case "win": {
-            console.log("WIN");
-            rl.close();
-            break;
-        }
-    }
-});
-
-setPlayers();
+    })
+})
