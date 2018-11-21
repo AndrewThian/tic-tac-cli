63915b2cfd91beae6e1fe569c7c61c1bd2f65fbd
AndrewThian <andrewbobbythian@gmail.com>
2018-11-21 12:22:08 +0800

    add e2e test for draw condition

---
+++ __tests__/e2e/draw.test.ts
@@ -0,0 +1,54 @@
+import { Game } from "../../lib/game"
+import { Board } from "../../lib/board"
+
+describe("e2e gameplay draw test suite", () => {
+    describe("draw conditions", () => {
+        beforeEach(() => {
+            // ignore print
+            console.log = jest.fn(() => {});
+        });
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+        test("game draw condition 3x3", () => {
+            const board = new Board(3);
+            const game = new Game(board);
+            const symbols: any = ["x", "o"]
+            const gameloop = [1,2,3,4,5,6,7,8,9]
+            
+            gameloop.map((loop, index) => {
+                game.play(loop, symbols[index % 2])
+            })
+        })
+        test("game draw condiiton 4x4", () => {
+            const boardSize = 4
+            const board = new Board(boardSize);
+            const game = new Game(board);
+            const symbols: any = ["x", "o"]
+            const gameloop = []
+
+            for (let i = 1; i <= (boardSize*boardSize); i++ ) {
+                gameloop.push(i)
+            }
+
+            gameloop.map((loop, index) => {
+                game.play(loop, symbols[index % 2])
+            })
+        })
+        test("game draw condiiton 5x5", () => {
+            const boardSize = 5
+            const board = new Board(boardSize);
+            const game = new Game(board);
+            const symbols: any = ["x", "o"]
+            const gameloop = []
+
+            for (let i = 1; i <= (boardSize*boardSize); i++ ) {
+                gameloop.push(i)
+            }
+
+            gameloop.map((loop, index) => {
+                game.play(loop, symbols[index % 2])
+            })
+        })
+    })
+})
