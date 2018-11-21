40831324fff553675b37f9112c21cccc70f0255f
AndrewThian <andrewbobbythian@gmail.com>
2018-11-21 14:22:46 +0800

    move gamestate into its own state.ts file

---
+++ lib/state.ts
@@ -0,0 +1,23 @@
+import { Player } from "./player";
+
+export class State {
+    playerPool: Player[];
+    currentTurn: number;
+
+    constructor(
+        p1: Player,
+        p2: Player,
+        currentTurn: number = 0,
+    ) {
+        this.playerPool = [p1, p2];
+        this.currentTurn = currentTurn;
+    }
+
+    currentPlayer = (): Player => {
+        return this.playerPool[this.currentTurn % 2];
+    };
+
+    nextTurn = (): void => {
+        this.currentTurn += 1;
+    };
+}
